dfc099dc6b3d1c61cd332834ff361318093b3c7d
JuvitaSaini09 <87228863+JuvitaSaini09@users.noreply.github.com>
2022-02-18 19:30:04 +0530

    docs : document updated

--- script.js
+++
@@ -1,8 +0,0 @@
-let bar=document.querySelector("#bars")
-let nav=document.querySelector(".nav-links")
-
-bar.addEventListener('click',()=>{
-    if(nav.style.display=="none") 
-    nav.style.display="block";
-    else nav.style.display="none";
-})
